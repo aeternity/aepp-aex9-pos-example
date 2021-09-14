56f7d29232e4ff99fc30599406c2c39422adbf57
Philipp Piwo <github@piwo.space>
2021-09-14 21:57:36 +0200

    feat(ae): replace cart with input field

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,47 +1,24 @@
 import {createStore} from 'vuex'
-import items from "@/assets/content/items.json";
 import config from "@/assets/content/config.json";
 
 const store = createStore({
   state() {
     return {
       page: 'SETUP',
-      cart: items.map(item => {
-        item.count = 0
-        return item
-      }),
+      requestTokenAmount: 0,
     }
   },
   mutations: {
-    resetStore(state) {
-      state.cart = items.map(item => {
-        item.count = 0
-        return item
-      })
-    },
-    addToCart(state, item) {
-      state.cart = state.cart.map(cartItem => {
-        if (cartItem.id === item.id) cartItem.count++
-        return cartItem
-      });
-    },
-    removeFromCart(state, item) {
-      state.cart = state.cart.map(cartItem => {
-        if (cartItem.count > 0 && cartItem.id === item.id) cartItem.count--
-        return cartItem
-      });
+    setRequestTokenAmount(state, amount) {
+      state.requestTokenAmount = amount
     },
     changePage(state, page) {
       state.page = page;
     },
   },
-
   getters: {
-    totalTokens(state) {
-      return state.cart.reduce((acc, item) => acc + item.price  * item.count, 0);
-    },
-    totalTokenWithoutDecimals(state, getters) {
-      return getters.totalTokens * Math.pow(10, config.decimals);
+    totalTokenWithoutDecimals(state) {
+      return state.requestTokenAmount * Math.pow(10, config.decimals);
     },
   }
 })
