dd3c10f2590236b355819be42c4ac273106a42c2
Philipp Piwo <github@piwo.space>
2021-09-16 14:01:01 +0200

    feat(config): dynamically set page title with token name

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createApp, h, markRaw} from 'vue'
 import page from 'page'
 import routes from './routes'
 import store from './utils/store';
+import config from "@/assets/content/config.json";
 
 const DefaultComponent = markRaw({
   render: () => h('div', 'Loading…')
@@ -17,6 +18,9 @@ const SimpleRouterApp = {
   },
 
   created() {
+    const capitalize = s => s && s[0].toUpperCase() + s.slice(1).toLowerCase()
+    document.title = `${capitalize(config.tokenName)}Pay | mPOS`
+
     for (let route in routes) {
       page(route, () => {
         this.ViewComponent = markRaw(require('./views/' + routes[route] + '.vue').default)
