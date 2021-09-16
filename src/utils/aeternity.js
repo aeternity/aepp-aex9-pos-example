efde649a3d5d61d90f873870647c217248992921
Philipp Piwo <github@piwo.space>
2021-09-16 14:32:59 +0200

    feat(config): move contract addresses to config

--- src/utils/aeternity.js
+++ src/utils/aeternity.js
@@ -1,19 +1,17 @@
 import {Node, Universal, MemoryAccount, Crypto} from '@aeternity/aepp-sdk/es';
 import FUNGIBLE_TOKEN_CONTRACT_INTERFACE from '../assets/contracts/fungible-token-custom-interface.aes';
 import POS_CONTRACT_INTERFACE from '../assets/contracts/pos-contract-interface.aes';
+import config from "@/assets/content/config.json";
 
 const MAINNET_URL = 'https://mainnet.aeternity.io';
 const COMPILER_URL = 'https://compiler.aepps.com';
-
-const TOKEN_CONTRACT_ADDRESS = 'ct_2t7TnocFw7oCYSS7g2yGutZMpGEJta6dq2DTX38SmuqmwtN6Ch';
-const POS_CONTRACT_ADDRESS = 'ct_213B8EVmEzuAG9FiqpvSQA4h4D9v8eNKLTYaBaDUVVp9fnY67A';
 
 const aeternity = {
   client: null,
   token: null,
   pos: null,
   ready: false,
-  posContractAddress: POS_CONTRACT_ADDRESS,
+  posContractAddress: config.posContractAddress,
 };
 
 aeternity.generateAccount = () => {
@@ -35,8 +33,8 @@ aeternity.init = async ({publicKey, secretKey}) => {
     nodes: [{name: 'mainnet', instance: await Node({url: MAINNET_URL})}],
     accounts: [MemoryAccount({keypair: {secretKey, publicKey}})],
   });
-  aeternity.token = await aeternity.client.getContractInstance(FUNGIBLE_TOKEN_CONTRACT_INTERFACE, {contractAddress: TOKEN_CONTRACT_ADDRESS})
-  aeternity.pos = await aeternity.client.getContractInstance(POS_CONTRACT_INTERFACE, {contractAddress: POS_CONTRACT_ADDRESS})
+  aeternity.token = await aeternity.client.getContractInstance(FUNGIBLE_TOKEN_CONTRACT_INTERFACE, {contractAddress: config.tokenContractAddress})
+  aeternity.pos = await aeternity.client.getContractInstance(POS_CONTRACT_INTERFACE, {contractAddress: aeternity.posContractAddress})
   aeternity.ready = true;
 };
 
