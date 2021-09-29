7328cbc4df3e78a721f6a1a2c778bd6d26377dc2
Philipp Piwo <github@piwo.space>
2021-09-29 10:49:29 +0200

    feat(setup): in create event check input and deploy contracts

--- src/utils/aeternity.js
+++ src/utils/aeternity.js
@@ -1,6 +1,8 @@
 import {Node, Universal, MemoryAccount, Crypto} from '@aeternity/aepp-sdk/es'
 import FUNGIBLE_TOKEN_CONTRACT_INTERFACE from '../assets/contracts/fungible-token-custom-interface.aes'
 import POS_CONTRACT_INTERFACE from '../assets/contracts/pos-contract-interface.aes'
+import FUNGIBLE_TOKEN_CONTRACT from '../assets/contracts/fungible-token-custom.aes'
+import POS_CONTRACT from '../assets/contracts/pos-contract.aes'
 
 const MAINNET_URL = 'https://mainnet.aeternity.io'
 const COMPILER_URL = 'https://compiler.aepps.com'
@@ -15,6 +17,8 @@ const aeternity = {
 aeternity.generateAccount = () => {
   return Crypto.generateKeyPair()
 }
+
+aeternity.isValidAddress = Crypto.isAddressValid
 
 aeternity.checkBalance = async () => {
   const address = await aeternity.client.address()
@@ -34,6 +38,22 @@ aeternity.initContracts = async(config) => {
   aeternity.pos = await aeternity.client.getContractInstance(POS_CONTRACT_INTERFACE, {contractAddress: config.posContractAddress})
 
   aeternity.ready = true
+}
+
+aeternity.deployContracts = async(name, symbol, decimals, mintedAmount, mintToAccount) => {
+  aeternity.token = await aeternity.client.getContractInstance(FUNGIBLE_TOKEN_CONTRACT)
+  await aeternity.token.methods.init(name, decimals, symbol, mintedAmount)
+  await aeternity.token.methods.transfer(mintToAccount, mintedAmount)
+
+  aeternity.pos = await aeternity.client.getContractInstance(POS_CONTRACT)
+  await aeternity.pos.methods.init(aeternity.token.deployInfo.address)
+
+  aeternity.ready = true
+
+  return {
+    posContractAddress: aeternity.pos.deployInfo.address,
+    tokenContractAddress: aeternity.token.deployInfo.address
+  }
 }
 
 aeternity.getTokenMetaInfo = () => aeternity.token.methods.meta_info().then(res => res.decodedResult)
